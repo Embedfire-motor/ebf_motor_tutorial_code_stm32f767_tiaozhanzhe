48d6235989feb74d2eecf5a4dc9f5e47bb69f3cc
hejiawen <hjw0415@outlook.com>
2020-12-09 10:28:59 +0800

    修复梯形加减速精度问题,修改错误注释

--- improve_part/stepper—梯形加减速实现/User/main.c
+++ improve_part/stepper—梯形加减速实现/User/main.c
@@ -24,11 +24,11 @@
 
 
 // 硬件决定速度的上限，软件算法决定是否可以达到上限
-__IO uint32_t set_speed  = 4000;         // 速度 单位为0.05rad/sec
+__IO uint32_t set_speed  = 2000;         // 速度 单位为0.1rad/sec
 // 加速度和减速度选取一般根据实际需要，值越大速度变化越快，加减速阶段比较抖动
 // 所以加速度和减速度值一般是在实际应用中多尝试出来的结果
-__IO uint32_t accel_val = 500;         // 加速度 单位为0.0.5rad/sec^2
-__IO uint32_t decel_val = 100;         // 减速度 单位为0.05rad/sec^2
+__IO uint32_t accel_val = 200;         // 加速度 单位为0.1rad/sec^2
+__IO uint32_t decel_val = 200;         // 减速度 单位为0.1rad/sec^2
 
 
 
@@ -48,6 +48,7 @@ int main(void)
 	printf("欢迎使用野火 电机开发板 步进电机 加减速正反旋转 例程\r\n");
 	/*步进电机初始化*/
 	stepper_Init();
+	MOTOR_EN(HIGH);
 	int i=0,dir_val=0;
 	while(1)
 	{     
